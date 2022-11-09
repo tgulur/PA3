9fea74b3791c9d74dc64acca7371f07d9934fd36
ViolinJ <115602262+ViolinJ@users.noreply.github.com>
2022-11-09 00:50:05 -0500

    Update plot_links.m

--- plot_links.m
+++ plot_links.m
@@ -9,5 +9,6 @@ y1 = [0 L1*sin(j1)];
 x2 = [L1*cos(j1) L1*cos(j1)+L2*cos(j1 + j2)];
 y2 = [L1*sin(j1) L1*sin(j1)+L2*sin(j1 + j2)];
 
-h = plot(x1,y1,x2,y2);
+h = plot(x1,y1,x2,y2,x2(length(x2)), y2(length(y2)), 'r*');
+h(3).LineWidth = 2;
 hold off
