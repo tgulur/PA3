809a0c1a7c166eeec6f87c339ac53c5ab807b7a5
Tejas Gulur <tgulur@gmail.com>
2022-11-08 22:33:13 -0500

    User input fixed
    user inputs fixed

--- inverse_kinematics.m
+++ inverse_kinematics.m
@@ -6,6 +6,10 @@ close all
 % Get the user input
 % points = 5;
 points = input('Type in how many points you are entering: ');
+while(~isnumeric(points) || points <= 1)
+    fprintf("Invalid input!\n")
+    points = input('Type in how many points you are entering: ');
+end 
 
 % Open figure 1 for our user input 
 figure(1)
@@ -14,41 +18,58 @@ figure(1)
 x_num = zeros(points,1); 
 y_num = zeros(points,1);
 
-
+% Set line property for the points
+line_property = plot(0,0,'k.-');
 % Loop thru amount of inputs and store them
 for i = 1:points
-    plot_circle(0,0,1);
-    plot_circle(0,0,1.5);
+    
+    % Plot circle function which gives the workspace
+    plot_circle(0,0, 0.5);
+    plot_circle(0,0, 1.5);
+    
+    % Gives the circles
     title(['Click  on ' num2str(points-i+1) ' points'])
     [x,y] = ginput(1);
-
-
-    x_num(i) = x;
-    y_num(i) = y;
-    if (x_num(i) == 0 && y_num(i) == 0)
-        continue
-    else 
-        plot(x_num,y_num, 'Color', 'k')
+    
+    % Initialize radioos (radius) and prompt user to select inside the
+    % workspace
+    radioos = sqrt(x^2 + y^2);
+    while( radioos < 0.5 || radioos > 1.5)
+        title(sprintf('Point must be in the workspace! Try again'))
+        [x,y] = ginput(1);
+        radioos = sqrt(x^2 + y^2);
     end 
 
-    
+    % Write to array
+    x_num(i) = x;
+    y_num(i) = y;
+
+    % Set array val
+    set(line_property, 'xdata', x_num(1:i), 'ydata', y_num(1:i));
+
+     
+
 end 
 
 % Convert the column vectors to a theta and rho that are defined in polar
 [theta,rho] = cart2pol(x_num, y_num);
 
-% figure(2)
 
-% Resulting curve  function returned
+
+figure(2)
+
+% Resulting curve  function returned w/ circles 
 [T, R]  = awesome_curve_fit(theta,rho);
+plot(x_num, y_num);
+plot(R.*cos(T), R.*sin(T));
+
+plot_circle(0,0,1);
+plot_circle(0,0,1.5);
+
+ 
 
 % Getting radius values for the curve 
-[j1, j2] =  spline_disc(T, R);
-
-% polarplot(theta,R)
-
-
-
+% [j1, j2] =  spline_disc(T, R);
 
 
 
